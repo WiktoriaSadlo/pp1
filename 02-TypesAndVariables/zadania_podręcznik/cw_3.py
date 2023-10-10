a176d3646436ca9bea02e48c6711927b11d449b0
WiktoriaSadlo <227960@student.uek.krakow.pl>
2023-10-10 23:34:47 +0200

    zadanka

---
+++ 02-TypesAndVariables/zadania_podręcznik/cw_3.py
@@ -0,0 +1,3 @@
+hours = int(input('Podaj liczbę godzin:'))
+salary = float(input('Podaj stawkę godzinową:'))
+print('Wynagrodzenie: ' + str(hours*salary))
---
+++ 02-TypesAndVariables/zadania_podręcznik/cw_5.py
@@ -0,0 +1,3 @@
+celsjusze = float(input('Podaj temperaturę w stopniach Celsjusza:'))
+fahrenheity = celsjusze*1.8 + 32
+print(str(celsjusze) + '°C to ' + str(fahrenheity) + '°F')
